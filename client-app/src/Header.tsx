05dc1ffe03e85565e546956fd0a10a5788a1f22d
takuto <tiharu717@gmail.com>
2024-06-30 09:26:37 +0900

    add routing

--- client-app/src/Header.tsx
+++ client-app/src/Header.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 export function Header() {
     return (
         <header className="header">
             <nav>
                 <ul className="ul">
-                    <li>Login</li>
+                    <li><Link to="/login">Login</Link></li>
                     <li>Register</li>
                 </ul>
             </nav>
--- client-app/src/Login.tsx
+++ client-app/src/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Login() {
+export function Login() {
     const [userId, setUserId] = useState('')
     const [password, setPassword] = useState('')
     
