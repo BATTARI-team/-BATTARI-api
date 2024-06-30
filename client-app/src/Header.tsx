54f6b6c4d92dcda9971b076c176ce0a2c90f0dc3
takuto <tiharu717@gmail.com>
2024-06-30 09:08:00 +0900

    ヘッダーのリストが横にならんと思ったら，cssをインポートしてないだけだった

---
+++ client-app/src/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';
+export function Header() {
+    return (
+        <header className="header">
+            <nav>
+                <ul className="ul">
+                    <li>Login</li>
+                    <li>Register</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ client-app/src/Login.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Login() {
+    const [userId, setUserId] = useState('')
+    const [password, setPassword] = useState('')
+    
+    return (
+        <>
+        </>
+    )
+}
