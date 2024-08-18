ba12dea554f0c99cc5bb2c417d75394040cc2c23
takuto <tiharu717@gmail.com>
2024-08-18 14:36:26 +0900

    fix port arg

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-var options = new WebApplicationOptions()
-{
-    Args = args,
-    ContentRootPath = Directory.GetCurrentDirectory(),
-    WebRootPath = "wwwroot"
-};
+var options = new WebApplicationOptions() { Args = args,
+                                            ContentRootPath =
+                                                Directory.GetCurrentDirectory(),
+                                            WebRootPath = "wwwroot" };
 var builder = WebApplication.CreateBuilder(options);
 
 // Add services to the container.
@@ -11,16 +9,15 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.WebHost.UseUrls(
-    "http://*:5050");
+builder.WebHost.UseUrls("http://*:" + args[0]);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+if (!app.Environment.IsDevelopment()) {
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for
+  // production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseSwagger();
@@ -32,8 +29,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default",
+                       pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
