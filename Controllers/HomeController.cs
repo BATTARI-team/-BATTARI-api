effd1b06c4f0a6f2637876feb12d1d4b032593f7
takuto1127 <tiharu717@gmail.com>
2024-11-22 12:48:23 +0900

    遭遇材料一覧を見れるページを作成した（自動更新が欲しいところだ・・）

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,11 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BATTARI_api.Models;
 using BATTARI_api.Repository.Data;
+using BATTARI_api.ViewModel;
 
 namespace BATTARI_api.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ISouguuService souguuService) : Controller
 {
     public IActionResult Index()
     {
@@ -21,5 +22,10 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult SouguuIncredients()
+    {
+        return View(new SouguuIncredientsViewModel(souguuService));
     }
 }
